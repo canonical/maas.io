68705c3b40b699048989392248d7e5e92fe88e70
Richard McCartney <richard@mccartney.io>
2015-04-16 16:21:47 +0200

    removing js for active class on load
    Signed-off-by: Richard McCartney <richard@mccartney.io>

--- static/js/jquery.accordion.js
+++ static/js/jquery.accordion.js
@@ -3,9 +3,6 @@ jQuery(document).ready(function($) {
     // Set accordion variable
     var accordion = $('.accordion');
 
-    // Sets the first accordion tab to active
-    accordion.find('.accordion__tab-title').first().addClass('active');
-
     accordion.on('click', '.accordion__tab-title', function(event) {
         event.preventDefault();
 
