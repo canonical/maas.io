54835a1b5323b662f49af1b6b2db395235e48a04
Richard McCartney <richard@mccartney.io>
2015-11-17 13:29:05 +0000

    Adding copy inputs JS

---
+++ static/js/jquery.command-line.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.command-line').each(function() {
+      var self = $(this),
+          copyButton = self.find('.js-copy-to-clipboard'),
+          commandInput = self.find('.command-line__input');
+
+      if (copyButton && commandInput) {
+        copyButton.on('click', function(e) {
+          e.preventDefault;
+          commandInput.select();
+
+          try {
+              var successful = document.execCommand('copy');
+              _gaq.push(['_trackEvent', 'Copy to clipboard', commandInput.get('value'), document.URL]);
+              _this.addClass('is-copied');
+              setTimeout(function(e) {
+                  _this.removeClass('is-copied');
+              }, 300);
+          } catch(err) {
+              console.log('Unable to copy');
+          }
+        });
+      }
+
+      if (commandInput) {
+          commandInput.on('click', function(e) {
+              this.select();
+          });
+      }
+    });
+});
