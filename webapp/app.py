a062993b42016adf1aef2f6226842fe9ddee0475
Robin Winslow <robin@robinwinslow.co.uk>
2019-07-23 15:23:49 +0100

    Tidy up code

--- webapp/app.py
+++ webapp/app.py
@@ -2,23 +2,19 @@
 A Flask application for maas.io
 """
 
-import os
 import flask
 
-from canonicalwebteam.discourse_docs import DiscourseDocs, DiscourseAPI
-from canonicalwebteam.discourse_docs.parsers import DocParser
+from canonicalwebteam.discourse_docs import (
+    DiscourseDocs,
+    DocParser,
+    DiscourseAPI,
+)
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.templatefinder import TemplateFinder
 from canonicalwebteam.search import build_search_view
 
 from webapp.feeds import get_rss_feed
 
-
-DISCOURSE_BASE_URL = "https://discourse.maas.io/"
-DOCS_TOPIC_ID = 25
-DOCS_CATEGORY_ID = 5
-DOCS_URL_PREFIX = "/docs"
-DOCS_TEMPLATE_PATH = "docs/document.html"
 
 app = FlaskBase(
     __name__,
@@ -27,24 +23,22 @@ app = FlaskBase(
     static_folder="../static",
 )
 
-app.config["SEARCH_API_KEY"] = os.getenv("SEARCH_API_KEY")
-app.config["SEARCH_API_URL"] = "https://www.googleapis.com/customsearch/v1"
-app.config["SEARCH_CUSTOM_ID"] = "009048213575199080868:i3zoqdwqk8o"
-
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/", view_func=template_finder_view)
 app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
 
-discourse_api = DiscourseAPI(base_url=DISCOURSE_BASE_URL)
+doc_parser = DocParser(
+    api=DiscourseAPI(base_url="https://discourse.maas.io/"),
+    index_topic_id=25,
+    url_prefix="/docs",
+)
 if app.debug:
-    discourse_api.session.adapters["https://"].timeout = 99
-
+    doc_parser.api.session.adapters["https://"].timeout = 99
 discourse_docs = DiscourseDocs(
-    api=discourse_api,
-    index_topic_id=DOCS_TOPIC_ID,
-    category_id=DOCS_CATEGORY_ID,
-    document_template=DOCS_TEMPLATE_PATH,
-    url_prefix=DOCS_URL_PREFIX,
+    parser=doc_parser,
+    document_template="docs/document.html",
+    category_id=5,
+    url_prefix="/docs",
 )
 discourse_docs.init_app(app)
 
@@ -78,6 +72,7 @@ def api():
     Show the static api page
     """
 
-    parser = DocParser(discourse_api, DOCS_TOPIC_ID, DOCS_URL_PREFIX)
-
-    return flask.render_template("docs/api.html", navigation=parser.navigation)
+    doc_parser.parse()
+    return flask.render_template(
+        "docs/api.html", navigation=doc_parser.navigation
+    )
