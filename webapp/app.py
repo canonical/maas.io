4807df0b71d95a85c65f957bdf441f2b5f69d198
Nick De Villiers <nick.devilliers@canonical.com>
2022-12-08 12:07:56 +0000

    Linting

--- webapp/app.py
+++ webapp/app.py
@@ -106,7 +106,7 @@ def api():
     definition_url = (
         "https://raw.githubusercontent.com"
         "/maas/maas-openapi-yaml/main/openapi2.yaml"
-        )
+    )
     openapi = parse_openapi(definition_url)
 
     doc_parser.parse()
--- webapp/openapi_parser.py
+++ webapp/openapi_parser.py
@@ -3,25 +3,26 @@ from yaml import load
 
 from yaml import Loader
 
-def parse_openapi(url: str):
-  definition = request.urlopen(url)
-  loaded_definition = load(definition, Loader)
-  tagged_definition = {}
 
-  for endpoint, method in loaded_definition["paths"].items():
-      if method != "parameters":
-          tag = loaded_definition["paths"][endpoint][method]["tags"][0]
-          if tag in tagged_definition:
-              if {
-                  endpoint: loaded_definition["paths"][endpoint]
-              } not in tagged_definition[tag]:
-                  tagged_definition[tag] = [
-                      *tagged_definition[tag],
-                      {endpoint: loaded_definition["paths"][endpoint]},
-                  ]
-          else:
-              tagged_definition[tag] = [
-                  {endpoint: loaded_definition["paths"][endpoint]}
-                  ]
-  
-  return tagged_definition
+def parse_openapi(url: str):
+    definition = request.urlopen(url)
+    loaded_definition = load(definition, Loader)
+    tagged_definition = {}
+
+    for endpoint, method in loaded_definition["paths"].items():
+        if method != "parameters":
+            tag = loaded_definition["paths"][endpoint][method]["tags"][0]
+            if tag in tagged_definition:
+                if {
+                    endpoint: loaded_definition["paths"][endpoint]
+                } not in tagged_definition[tag]:
+                    tagged_definition[tag] = [
+                        *tagged_definition[tag],
+                        {endpoint: loaded_definition["paths"][endpoint]},
+                    ]
+            else:
+                tagged_definition[tag] = [
+                    {endpoint: loaded_definition["paths"][endpoint]}
+                ]
+
+    return tagged_definition
