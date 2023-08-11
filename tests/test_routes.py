dd79ece1372bf7162c154ac2d45a1d26339f4efe
Anton Troyanov <anton.troyanov@canonical.com>
2023-08-11 19:11:41 +0300

    feat: return metadata for `go get`

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -26,6 +26,34 @@ class TestRoutes(unittest.TestCase):
 
         self.assertEqual(self.client.get("/not-found-url").status_code, 404)
 
+    def test_gomod(self):
+        """
+        When given /core.* with go-get=1 query parameter,
+        we should return gomod
+        """
+
+        self.assertEqual(self.client.get("/core?go-get=1").status_code, 200)
+        self.assertEqual(
+            self.client.get("/core/maas?go-get=1").status_code, 200
+        )
+        self.assertEqual(
+            self.client.get("/core/maas/src?go-get=1").status_code, 200
+        )
+
+    def test_gomod_not_found(self):
+        """
+        When given /core.* without go-get=1 query parameter,
+        we should return a 404 status code
+        """
+
+        self.assertEqual(self.client.get("/core").status_code, 404)
+        self.assertEqual(self.client.get("/core/maas").status_code, 404)
+        self.assertEqual(self.client.get("/core/maas/src").status_code, 404)
+        self.assertEqual(self.client.get("/core/maas?go-get").status_code, 404)
+        self.assertEqual(
+            self.client.get("/core/maas/src?go-get=0").status_code, 404
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
